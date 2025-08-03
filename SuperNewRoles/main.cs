ea1da70c94befaa4981c796a5b8f15d09c49d2f3
Sakurander <sakurander77@gmail.com>
2025-08-04 01:35:20 +0900

    デバッグ用の記述を削除

--- SuperNewRoles/main.cs
+++ SuperNewRoles/main.cs
@@ -127,8 +127,6 @@ public partial class SuperNewRolesPlugin : BasePlugin
             }
         });
 
-        SNRRandomCenter.InitState(123456);
-
         Logger.LogInfo("Waiting for Harmony patch");
         if (ModHelpers.IsAndroid())
         {
